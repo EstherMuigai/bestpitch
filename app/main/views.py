dba91237e7a34dc1363fe70350e9791291330884
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-04-19 22:11:43 +0300

    Set up the database connection.
    Installation of SQLAlchemy and psycopg2 Postgres driver. Also setting up the URI and initializing the database

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,11 @@ from flask import render_template,request,redirect,url_for
 from . import main
 
 @main.route('/')
-def index():
+def landingpage():
 
     return render_template('landingpage.html')
+
+@main.route('/timeline')
+def timeline():
+
+    return render_template('timeline.html')
