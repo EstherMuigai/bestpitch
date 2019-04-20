003a991eb33e1ba3b97298520812fa509f6eac64
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-04-20 14:07:26 +0300

    Add an authentication blueprint called auth.
    Import werkzeug.security from flask and set up two functions, one to generate a password and one to check the password against the generated hash. Also, setting up a test for the password and registering the authentication blueprint to the create_app function that initializes the entire app in app/__init__.py file.

---
+++ app/models.py
@@ -0,0 +1,48 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
+    comment_id = db.Column(db.Integer,db.ForeignKey('comment.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    category = db.Column(db.String(255))
+    comment_id = db.Column(db.Integer,db.ForeignKey('comment.id'))
+    users = db.relationship('User',backref = 'pitch',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.id}'
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    users = db.relationship('User',backref = 'comment',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.id}'
